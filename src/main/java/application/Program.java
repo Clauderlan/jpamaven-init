10ea7120c31a84d42d31c1cabeae36e69fdd9520
c7 <89857887+Clauderlan@users.noreply.github.com>
2023-04-27 17:17:01 -0300

    select, insert and delete examples with JPA

--- src/main/java/application/Program.java
+++ src/main/java/application/Program.java
@@ -2,13 +2,34 @@ package application;
 
 import entities.Person;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 public class Program {
     public static void main(String[] args) {
 
-        Person person1 = new Person(1, "Carlos da Silva", "carlos@gmail.com");
-        Person person2 = new Person(1, "Joaquim da Silva", "joaquim@gmail.com");
-        Person person3 = new Person(1, "Rafael da Silva", "rafael@gmail.com");
+        EntityManagerFactory managerFactory = Persistence.createEntityManagerFactory("exemplo-jpa");
+        EntityManager manager = managerFactory.createEntityManager();
 
+        // Insert with JPA - Hibernate
+        manager.getTransaction().begin();
+        Person person = new Person(null, "Claudio" , "claudigo@gmail.com");
+        manager.persist(person);
+        manager.getTransaction().commit();
 
+        // Select with JPA - Hibernate - SelectById
+        Person p = manager.find(Person.class, person.getId());
+        System.out.println(p);
+
+        // Delete with JPA - Hibernate, PT-BR -
+
+        manager.getTransaction().begin();
+        manager.remove(p);
+        manager.getTransaction().commit();
+
+        System.out.println("Good.");
+        manager.close();
+        managerFactory.close();
     }
 }
--- src/main/java/entities/Person.java
+++ src/main/java/entities/Person.java
@@ -1,12 +1,20 @@
 package entities;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Person {
 
-
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
     private Integer id;
     private String name;
     private String email;
 
+    public Person(){}
     public Person(Integer id, String name, String email) {
         this.id = id;
         this.name = name;
