46c117ef3e82275259158666d7f75d1c0e3e7e62
c7 <89857887+Clauderlan@users.noreply.github.com>
2023-04-27 16:39:13 -0300

    inital config

---
+++ src/main/java/application/Program.java
@@ -0,0 +1,14 @@
+package application;
+
+import entities.Person;
+
+public class Program {
+    public static void main(String[] args) {
+
+        Person person1 = new Person(1, "Carlos da Silva", "carlos@gmail.com");
+        Person person2 = new Person(1, "Joaquim da Silva", "joaquim@gmail.com");
+        Person person3 = new Person(1, "Rafael da Silva", "rafael@gmail.com");
+
+
+    }
+}
